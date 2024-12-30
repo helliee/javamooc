baf660f0305ad343dc9801190363b2185ce75444
Ellie H. <ellie@lly.dev>
2024-12-29 19:23:21 -0600

    +2 exercises

---
+++ src/Part2Section4/NumeroUno.java
@@ -0,0 +1,11 @@
+package Part2Section4;
+
+public class NumeroUno {
+    public static void main(String[] args) {
+        System.out.println(numeroUno());
+    }
+
+    public static int numeroUno() {
+        return 1;
+    }
+}
---
+++ src/Part2Section4/Word.java
@@ -0,0 +1,12 @@
+package Part2Section4;
+
+public class Word {
+    public static void main(String[] args) {
+        System.out.println(word());
+    }
+
+    public static String word() {
+
+        return "Hello, word!";
+    }
+}
