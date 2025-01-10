08dbf6c1dceb5fd93626ff7c51dca71327264ded
ellie <ellie@lly.dev>
2025-01-09 19:07:37 -0600

    finished exercise

--- src/Part2Section4/Greatest.java
+++ src/Part2Section4/Greatest.java
@@ -2,6 +2,11 @@ package Part2Section4;
 
 public class Greatest {
     public static void main(String[] args) {
+        int answer = greatest(20, 7, 399);
+        System.out.println("Greatest: " + answer);
+    }
 
+    public static int greatest(int number1, int number2, int number3) {
+        return Math.max((Math.max(number1, number2)), number3);
     }
 }
