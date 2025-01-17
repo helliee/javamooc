6d94d14e5a250a4d80db69e7239953e8394cdac0
Ellie H. <ellie@lly.dev>
2025-01-16 20:01:14 -0600

    started exercise

--- src/Part2Section4/StarSign.java
+++ src/Part2Section4/StarSign.java
@@ -1,4 +1,16 @@
 package Part2Section4;
 
 public class StarSign {
+    public static void main(String[] args) {
+        printStars(5);
+        printStars(3);
+        printStars(9);
+    }
+
+    public static void printStars(int number) {
+        for (int i = 0; i < number; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+    }
 }
